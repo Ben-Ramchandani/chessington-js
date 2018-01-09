c087eb94537b4280739eb03cb35d46e063104caa
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2018-01-09 11:18:28 +0000

    Bishops move

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -6,6 +6,22 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let moves = []
+        for (let i = 1; i < board.board.length; i++) {
+            moves.push({ row: this.position.row + i, col: this.position.col + i }, {
+                row: this.position.row + i,
+                col: this.position.col - i
+            }, { row: this.position.row - i, col: this.position.col + i }, {
+                row: this
+                    .position.row - i,
+                col: this.position.col - i
+            })
+        }
+        moves = moves.filter(item => item.row >= 0 &&
+            item.row < board.board.length &&
+            item.col >= 0 &&
+            item.col < board.board.length
+        )
+        return moves
     }
 }
